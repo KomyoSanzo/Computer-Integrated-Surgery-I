ed0e5e5f9d59e3b0953749e1c611fe849f3bdf70
KomyoSanzo <wwangsta@gmail.com>
2015-10-11 22:14:02 -0400

    made changes at the end by taking the transpose, adjusting matrix size, and overall bug changes

--- C2C.m
+++ C2C.m
@@ -11,18 +11,17 @@ adjustedA = listOfPointsA;
 adjustedB = listOfPointsB;
 
 for i = 1:numberOfPoints
-    adjustedA(i,:) = adjustedA(i,:) - averageA.';   
-    adjustedB(i,:) = adjustedB(i,:) - averageB.';   
+    adjustedA(i,:) = adjustedA(i,:) - averageA;   
+    adjustedB(i,:) = adjustedB(i,:) - averageB;   
 end
 
 %Computing H
 
-H = zeros(size(listOfPointsA));
+H = zeros(3);
 for i = 1:numberOfPoints
-    H = H + [adjustedA(i,1)*adjustedB(i,1) adjustedA(i,1)*adjustedB(i,2) adjustedA(i,1)*adjustedB(i,3);
-        adjustedA(i,2)*adjustedB(i,1) adjustedA(i,2)*adjustedB(i,2) adjustedA(i,2)*adjustedB(i,3);
-        adjustedA(i,3)*adjustedB(i,1) adjustedA(i,3)*adjustedB(i,2) adjustedA(i,3)*adjustedB(i,3)
-        ];
+    test = [adjustedA(i,1)*adjustedB(i,1) adjustedA(i,1)*adjustedB(i,2) adjustedA(i,1)*adjustedB(i,3); adjustedA(i,2)*adjustedB(i,1) adjustedA(i,2)*adjustedB(i,2) adjustedA(i,2)*adjustedB(i,3); adjustedA(i,3)*adjustedB(i,1) adjustedA(i,3)*adjustedB(i,2) adjustedA(i,3)*adjustedB(i,3)];
+    
+    H = H + test;
 end
 
 delta = [H(2,3)-H(3,2) H(3,1)-H(1,3) H(1,2)-H(2,1)].';
@@ -37,7 +36,8 @@ R = [q(1)*q(1)+q(2)*q(2)-q(3)*q(3)-q(4)*q(4) 2*(q(2)*q(3)-q(1)*q(4)) 2*(q(2)*q(4
     2*(q(2)*q(3)+q(1)*q(4)) q(1)*q(1)-q(2)*q(2)+q(3)*q(3)-q(4)*q(4) 2*(q(3)*q(4)-q(1)*q(2));
     2*(q(2)*q(4)-q(1)*q(3)) 2*(q(3)*q(4)+q(1)*q(2)) q(1)*q(1)-q(2)*q(2)-q(3)*q(3)+q(4)*q(4)];
 
-p = averageB - R*averageA;
+
+p = averageB.' - R*averageA.';
 
 end
 
