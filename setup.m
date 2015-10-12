f9b893c0765c97625a6497afd31212954c7e0f41
eschwa29 <eschwa29@jhu.edu>
2015-10-11 20:51:21 -0400

    Updated

--- setup.m
+++ setup.m
@@ -14,4 +14,25 @@ function setup(name)
         c = M((aN+dN):(aN+dN+cN), :,:);
     end
 
+    function F = parseReadings(filename)
+        M = csvread(filename, 1, 0);
+        info = fileread(filename);
+        info = strsplit(info(1,:), ',');
+        dN = str2double(info(1));
+        aN = str2double(info(2));
+        cN = str2double(info(3));
+        fN = str2double(info(4));
+        
+        for n = 1:fN
+            frame(n) = 
+        end
+        
+        frame(1,:) 
+        d = M(1:dN, :,:);
+        a = M(dN:(aN+dN), :,:);
+        c = M((aN+dN):(aN+dN+cN), :,:);
+    end
+
+
+
 end
