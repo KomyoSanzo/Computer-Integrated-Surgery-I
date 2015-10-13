50b2650fb110616084e6dcded41c8c4a4ea7ba86
KomyoSanzo <wwangsta@gmail.com>
2015-10-13 11:44:02 -0400

    Modified so that it displays the average errors and allows unknown file testing

--- Test.m
+++ Test.m
@@ -12,31 +12,64 @@ function Test(name)
     %returns value very close to original points. Ensures correct
     %rotation and displacement were returned.
     
+    
+    Caverage = 0;
+    Daverage = 0;
+    Aaverage = 0;
+    
     for n = 1:size(readings, 1)
         [RD, pD] = CloudToCloud(dCloud, readings{n,1});
         [RA, pA] = CloudToCloud(aCloud, readings{n,2});
         [RC, pC] = CloudToCloud(cCloud, readings{n,3});
+        
+        averageError = 0;
         for i = 1:size(cCloud)
             difference = readings{n,3}(i,:) - (RC*cCloud(i, :).' + pC).';
             difference = sqrt(difference*difference');
+            averageError = averageError + difference;
             if difference > 20
                 disp('Error: C frame transformation is incorrect');
             end
         end
+        averageError = averageError/length(cCloud);
+        Caverage = Caverage+averageError;
+        
+        averageError = 0;
         for i = 1:size(aCloud)
             difference = readings{n,2}(i,:) - (RA*aCloud(i, :).' + pA).';
-            difference = sqrt(difference*difference');
+            difference = sqrt(difference*difference');        
+            averageError = averageError + difference;
+            
             if difference > 1
                 disp('Error: A frame transformation is incorrect');
             end
         end
+        averageError = averageError/length(aCloud);
+        Aaverage = Aaverage + averageError;
+        
+        averageError = 0;
         for i = 1:size(dCloud)
             difference = readings{n,1}(i,:) - (RD*dCloud(i, :).' + pD).';
-            difference = sqrt(difference*difference');
+            difference = sqrt(difference*difference');     
+            averageError = averageError + difference;
+           
             if difference > 1
                 disp('Error: B frame transformation is incorrect');
             end
         end
+        
+        averageError = averageError/length(dCloud);
+        Daverage = Daverage+averageError;
+      
     end
-    OutputTest(name, 10);
+    
+    disp(['Average error for C Cloud across all frames is: ', num2str(Caverage/size(readings,1))]);
+    disp(['Average error for A Cloud across all frames is: ', num2str(Aaverage/size(readings,1))]);
+    disp(['Average error for D Cloud across all frames is: ', num2str(Daverage/size(readings,1))]);
+       
+    DebugOrUnknown = strsplit(name, '-');
+    if (strcmp(DebugOrUnknown(2), 'debug'))
+        OutputTest(name, 10);
+    end
+    
 end
