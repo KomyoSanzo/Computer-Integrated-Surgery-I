d6cd1998f972c094b0935e91685c1110d681ab95
KomyoSanzo <wwangsta@gmail.com>
2015-10-20 23:05:08 -0400

    Changed to accept cells

--- ScaleToBox.m
+++ ScaleToBox.m
@@ -1,10 +1,13 @@
 function [ u ] = ScaleToBox(x)
-u = zeros(size(x));
+    u = cell(size(x))
+    
+    for j = length(u)
+        u{j} = zeros(size(x));
 
-for i = 1:3
-    min = min(q(:,i));
-    max = max(q(:,i));
-    u(:,i) = (x-x_min)/(x_max-x_min);
-end
-
+        for i = 1:3
+            min = min(x{j}(:,i));
+            max = max(x{j}(:,i));
+            u{j}(:,i) = (x-x_min)/(x_max-x_min);
+        end
+    end
 end
