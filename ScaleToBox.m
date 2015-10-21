0199ee93a93d553da01b33c7842dfc7944587afc
KomyoSanzo <wwangsta@gmail.com>
2015-10-20 22:40:12 -0400

    Modified ScaleToBox so that it doesn't require additional parameters

--- ScaleToBox.m
+++ ScaleToBox.m
@@ -1,3 +1,10 @@
-function [ u ] = ScaleToBox(x, x_min, x_max)
-u = (x-x_min)/(x_max-x_min)
+function [ u ] = ScaleToBox(x)
+u = zeros(size(x));
+
+for i = 1:3
+    min = min(q(:,i));
+    max = max(q(:,i));
+    u(:,i) = (x-x_min)/(x_max-x_min);
+end
+
 end
