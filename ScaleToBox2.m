5749338b3ad0a9b156c403490e24cc2c7f7d3b22
KomyoSanzo <wwangsta@gmail.com>
2015-10-21 17:35:08 -0400

    modified scaletobox2.m

---
+++ ScaleToBox2.m
@@ -0,0 +1,28 @@
+function [ u ] = ScaleToBox2(x)
+u = cell(size(x));
+
+for i = 1:length(x)
+    u{i} = zeros(size(x{i}));
+end
+
+
+for i = 1:3
+    global_min = x{1}(1,i);
+    global_max = x{1}(1,i);
+    
+    for j = 1:length(x)
+        if max(x{j}(:,i)) > global_max
+            global_max = max(x{j}(:,i));
+        end
+        
+        if min(x{j}(:,i)) < global_min
+            global_min = min(x{j}(:,i));
+        end
+        u{j}(:,i) = (x{j}(:,i)-global_min)/(global_max-global_min);
+    end
+    
+end
+
+
+
+
