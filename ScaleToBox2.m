88501abcd7a919389115b77af589accb7b33c6d8
KomyoSanzo <wwangsta@gmail.com>
2015-10-22 01:20:12 -0400

    Returns the min and max

--- ScaleToBox2.m
+++ ScaleToBox2.m
@@ -1,4 +1,4 @@
-function [ u ] = ScaleToBox2(x)
+function [ u, min, max ] = ScaleToBox2(x)
 u = cell(size(x));
 
 for i = 1:length(x)
@@ -7,20 +7,20 @@ end
 
 
 for i = 1:3
-    global_min = x{1}(1,i);
-    global_max = x{1}(1,i);
+    min = x{1}(1,i);
+    max = x{1}(1,i);
     
     for j = 1:length(x)
-        if max(x{j}(:,i)) > global_max
-            global_max = max(x{j}(:,i));
+        if max(x{j}(:,i)) > max
+            max = max(x{j}(:,i));
         end
         
-        if min(x{j}(:,i)) < global_min
-            global_min = min(x{j}(:,i));
+        if min(x{j}(:,i)) < min
+            min = min(x{j}(:,i));
         end
     end
     for j = 1:length(x)
-        u{j}(:,i) = (x{j}(:,i)-global_min)/(global_max-global_min);
+        u{j}(:,i) = (x{j}(:,i)-min)/(max-min);
     end
 end
 
